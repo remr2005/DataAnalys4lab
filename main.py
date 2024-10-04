61189de6fc34587fb0dd4f419174bb931a31d9e1
remr2005 <morpeh2005ka@gmail.com>
2024-10-04 20:02:01 +0300

    чёто высрал(ОНО НЕ РАБОТАЕТ БЛЯТЬ)

--- main.py
+++ main.py
@@ -2,35 +2,44 @@ import random
 from math import sqrt, pi, sin, cos
 from dsmltf import gradient, gradient_descent, minimize_stochastic
 import matplotlib.pyplot as plt
+from statistics import mean
 
+# какой то коэфициент)))
+dt = 2*pi/1000
+# иксы
 x = list()
-def furie2(k,a):
-    return a[0]+a[1]*cos(a[2]*k+a[8])+a[3]*cos(a[4]*k+a[9])+a[5]*cos(a[6]*k*2+a[10])+cos(a[7]*2*k+a[11])
+# функция фурье 
 def furie(k,a):
-    return a[0] + a[1]*cos(k) + a[2]* sin(k) + a[3]*cos(2*k) + a[4]*sin(2*k)
+    return a[0]+a[1]*cos(a[2]*dt*k) + a[3]*sin(a[2]*dt*k)+a[4]*cos(a[5]*dt*k)+a[6]*sin(a[5]*dt*k)
 
+# функция ошибки для обычного градиентного спуска
 def F(a:list) -> float:
     global x
-    return sum([abs(x[j]-furie(j,a)) for j in range(500)])
+    return sum([(x[j]-furie(j,a))**2 for j in range(500)])
 
+# функция ошибки для стохатического градиентного спуска
 def f(i,a):
     global x
-    return abs(x[i]-furie(i,a))
-
+    return (x[i]-furie(i,a))**2
+ 
 def main():
-    k=30
-    dt=2*pi/1000
+    # какие то коэфицинты для создания ряда
+    k=1
+    global dt
     omega=1000/k
     L=k/100
     global x
     x = [0,(-1)**k * dt]
     for i in range(2,500):
         x.append(x[i-1]*(2+dt*L*(1-x[i-2]**2))- x[i-2]*(1+dt**2+dt*L*(1-x[i-2]**2))+dt**2*sin(omega*dt))  
-    a0 = gradient_descent(F,[0,0,0,0,0])
-    a1 = minimize_stochastic(f,[i for i in range(500)],[0]*500,[0,0,0,0,0])
-    print(a0[0],a1[0])
-    print(a0[1],a1[1])
+    # вычисляем коэфициенты
+    a0 = gradient_descent(F,[0]*7)
+    a1 = minimize_stochastic(f,[i for i in range(500)],[0]*500,[0]*7)
+    print(a0[0],a0[0])
+    print(a1[0],a1[1])
+    # ряд из 500 значений
     base = [i for i in range(500)]
+    # рисуем графики
     plt.plot(base, x, label='Изначальная функция', marker='o')
     plt.plot(base, [furie(i,a0[0]) for i in range(500)], label=f'Градиентный спуск', linestyle='-')
     plt.plot(base, [furie(i,a1[0]) for i in range(500)], label=f'Стохатичный градиентный спуск', linestyle='-')
@@ -39,7 +48,6 @@ def main():
     plt.legend()
     plt.grid(True)
     plt.show()
-
 
 if __name__ == "__main__":
     main()
